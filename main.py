1769ddca2e3bccf38e544440444f8d357b3126d9
Neko-Sato <hisatosi35350@gmail.com>
2023-01-17 18:40:36 +0900

    main

---
+++ main.py
@@ -0,0 +1,53 @@
+from m5stack import *
+import network
+import zaiseki
+
+def connect_wifi(*SSID_PASSs):
+  sta_if = network.WLAN(network.STA_IF)
+  sta_if.active(True)
+
+  SSIDs = [i[0].decode() for i in sta_if.scan()]
+
+  for SSID_PASS in SSID_PASSs:
+    if SSID_PASS[0] not in SSIDs:
+      continue
+    lcd.clear(lcd.BLACK)
+    lcd.text(lcd.CENTER, lcd.CENTER, "Connecting\n" + SSID_PASS[0])
+    sta_if.connect(*SSID_PASS)
+    while not sta_if.isconnected():
+      pass
+    lcd.clear(lcd.BLACK)
+    lcd.text(lcd.CENTER, lcd.CENTER, "Connected")
+    break
+  else:
+    raise
+
+connect_wifi(
+  ('iPhone', 'nekoneko'),
+  ('aterm-2d3596-g', '36816a76be0c5'),
+  ('Buffalo-G-F048', 's7vyvmhb8jcrj'),
+  ("aterm-b28299-g", "60898de9a1578"),
+)
+
+
+client_id = "4yew5dxbp443egaxc9smbu3xnpygvud9x66rw243yq2tm9c3ruqq35u7bu1bq7gp"
+authentication = {
+    "email": "22im0082@i-u.ac.jp",
+    "pc_login_pass":"DLkkiqCijot",
+}
+
+def attendance(isPresence):
+  lcd.clear(lcd.BLACK)
+  lcd.text(lcd.CENTER, lcd.CENTER, 'change...')
+  zaiseki.execute(client_id, authentication, isPresence)
+  lcd.clear(lcd.BLACK)
+  lcd.text(lcd.CENTER, lcd.CENTER, 'Attendance' if isPresence else "Absence")
+
+
+isPresence = True
+attendance(isPresence)
+
+while True:
+  if btnA.isPressed():
+    isPresence = not isPresence
+    attendance(isPresence)
---
+++ zaiseki.py
@@ -0,0 +1,93 @@
+import gc
+try:
+  import socket
+except:
+  import usocket as socket
+import ssl
+import re
+import sys
+MICROPY = sys.implementation.name == "micropython"
+del sys
+
+def get_conn():
+  sock = socket.socket()
+  sock.connect(("zaiseki.jp", 443))
+  try:
+    sock = ssl.create_default_context().wrap_socket(sock, server_hostname="zaiseki.jp")
+  except:
+    sock = ssl.wrap_socket(sock, server_hostname="zaiseki.jp")
+  return sock
+
+def __execute(client_id, authentication, isPresence):
+  #######
+  conn = get_conn()
+  f = conn if MICROPY else conn.makefile("rwb")
+  f.write(b"GET /client/%s/login HTTP/1.1\r\n" % client_id.encode())
+  f.write(b"Host: zaiseki.jp\r\n")
+  f.write(b"\r\n")
+  if not MICROPY:
+    f.flush()
+  r = f.read()
+  session_id = re.search(rb"PHPSESSID=[^;]+;", r).group(0)[:-1]
+  token = re.search(rb"token\" value=\"[^\"]*\"", r).group(0)[14:-1]
+  del conn, f, r
+  gc.collect()
+
+  #######
+  conn = get_conn()
+  f = conn if MICROPY else conn.makefile("rwb")
+  d = b"email=%s&pc_login_pass=%s&token=%s" % (authentication["email"].encode(), authentication["pc_login_pass"].encode(), token)
+  f.write(b"POST /client/%s/login HTTP/1.1\r\n" % client_id.encode())
+  f.write(b"Host: zaiseki.jp\r\n")
+  f.write(b"Cookie: %s\r\n" % session_id)
+  f.write(b"Content-Type: application/x-www-form-urlencoded\r\n")
+  f.write(b"Content-Length: %d\r\n" % len(d))
+  f.write(b"\r\n")
+  f.write(d)
+  if not MICROPY:
+    f.flush()
+  r = f.read()
+  session_id = re.search(rb"PHPSESSID=[^;]+;", r).group(0)[:-1]
+  del conn, f, r
+  gc.collect()
+
+  #######
+  conn = get_conn()
+  f = conn if MICROPY else conn.makefile("rwb")
+  f.write(b"GET /client/%s HTTP/1.1\r\n" % client_id.encode())
+  f.write(b"Host: zaiseki.jp\r\n")
+  f.write(b"Cookie: %s\r\n" % session_id)
+  f.write(b"\r\n")
+  if not MICROPY:
+    f.flush()
+  r = f.read()
+  conn.close()
+  token = re.search(rb"token\" value=\"[^\"]*\"", r).group(0)[14:-1]
+  del conn, f, r
+  gc.collect()
+
+  #######
+  conn = get_conn()
+  f = conn if MICROPY else conn.makefile("rwb")
+  d = b"status_id=%d&destination=&back_date=&back_time=&back_flg=0&member=&token=%s" % (60180 if isPresence else 60182, token)
+  f.write(b"POST /client/%s/zaiseki/update HTTP/1.1\r\n" % client_id.encode())
+  f.write(b"Host: zaiseki.jp\r\n")
+  f.write(b"Cookie: %s\r\n" % session_id)
+  f.write(b"Content-Type: application/x-www-form-urlencoded\r\n")
+  f.write(b"Content-Length: %d\r\n" % len(d))
+  f.write(b"\r\n")
+  f.write(d)
+  if not MICROPY:
+    f.flush()
+  else:
+    f.readline()
+  del conn, f
+  gc.collect()
+
+def execute(client_id, authentication, isPresence):
+  while True:
+    try:
+      __execute(client_id, authentication, isPresence)
+      break
+    except:
+      pass
